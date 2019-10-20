605d1242c402f1171021d186b5d405a23e79192f
john <Johnaellis0@gmail.com>
2019-10-20 14:22:14 -0700

    runs board_parser test on 8 available test board image data segments.

--- board_parser_test.py
+++ board_parser_test.py
@@ -13,28 +13,30 @@ parser = board_parser.BoardParser()
 board = np.zeros((1,800,800,3))
 
 # Tests the chess board classifier
-for r in range(1,65):
-  img = Image.open(cpaib_rel_path + test_data_path + test_iter + '/opencv_frame1_'+str(r)+'.jpg')
-  img = np.array(img)
-  img = np.reshape(img,(100,100,3))
-  #y = image.img_to_array(img)
+for test_iter in range(0, 8):
+  # test_iter cycles over available test images
+  for r in range(1,65):
+    img = Image.open(cpaib_rel_path + test_data_path + str(test_iter) + '/opencv_frame1_'+str(r)+'.jpg')
+    img = np.array(img)
+    img = np.reshape(img,(100,100,3))
+    #y = image.img_to_array(img)
 
-  #print(y.shape)
-  #print(img.shape)
+    #print(y.shape)
+    #print(img.shape)
 
-  # plt.imshow(img)
-  # plt.show()
-  img = np.reshape(img,(1,100,100,3))
-  # print(img.shape)
+    # plt.imshow(img)
+    # plt.show()
+    img = np.reshape(img,(1,100,100,3))
+    # print(img.shape)
 
-  
-  #pred = parser.parse_single_square(img)
-  #print(pred)
+    
+    #pred = parser.parse_single_square(img)
+    #print(pred)
 
-  x = ((r-1) // 8 + 1) * 100
-  y = ((r-1) % 8 + 1) * 100
+    x = ((r-1) // 8 + 1) * 100
+    y = ((r-1) % 8 + 1) * 100
 
-  board[0][x-100:x, y-100:y] = img
-  
-pred = parser.parse_board_image(board)
-print(pred)
+    board[0][x-100:x, y-100:y] = img
+
+  pred = parser.parse_board_image(board)
+  print(pred)
