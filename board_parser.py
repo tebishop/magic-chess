5fbe6620c2aaf35db345b9ad232da3dc6a601c31
john <Johnaellis0@gmail.com>
2019-10-20 14:18:32 -0700

    Adds board parser and tests.

---
+++ board_parser.py
@@ -0,0 +1,87 @@
+import keras
+import os
+import numpy as np
+
+root_dir = os.path.dirname(__file__)
+
+cpaib_abs_path = root_dir + '/Chess-Playing-AI-Bot/'
+# Takes a 800x800 image and spits out FEN codes
+
+class BoardParser:
+  def __init__(self):
+    self.opt = keras.optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=None, decay=0.0)
+    self.piece_detection_model = keras.models.load_model(cpaib_abs_path + 'saved_models/trained_model_final_bs32_225_dg.h5')
+
+    self.piece_detection_model.compile(
+      loss='binary_crossentropy',
+      optimizer=self.opt,
+      metrics=['accuracy'])
+
+    self.piece_detection_model.summary()
+
+
+  def parse_board_image(self, board_image):
+    """
+    Pass me a 1x800x800x3 and I'll give you a FEN
+    """
+    fen_code = ''
+
+
+    # assume that image is oriented 8:1 and a:h
+    for x in range (1, 9):
+      empty_ctr = 0
+      for y in range(1, 9):
+        square_img = np.reshape(board_image[0, x*100 - 100:x*100, y*100 -100: y*100], (1,100,100,3))
+        occupant = self.parse_single_square(square_img)
+        if occupant == 'E':
+          empty_ctr += 1
+          if y<8:
+            continue
+        if empty_ctr:
+          fen_code += f'{empty_ctr}'
+          empty_ctr = 0
+        if occupant != 'E':
+          fen_code += f'{occupant}'
+      if x < 8:
+        fen_code += '/'
+
+    # Extra bit of starting code to 
+    first_move_trailer = ' w KQkq - 0 1'
+    fen_code += first_move_trailer
+    
+    return fen_code
+
+  def parse_single_square(self, square_image):
+    classes = None
+
+    y = self.piece_detection_model.predict_classes(square_image)
+
+
+    if y==0:
+      classes='E' # Empty
+    elif y==1:
+      classes='B'
+    elif y==2:
+      classes='K'
+    elif y==3:
+      classes='N'
+    elif y==4:
+      classes='P'
+    elif y==5:
+      classes='Q'
+    elif y==6:
+      classes='R'
+    elif y==7:
+      classes='b'
+    elif y==8:
+      classes='k'
+    elif y==9:
+      classes='n'
+    elif y==10:
+      classes='p'
+    elif y==11:
+      classes='q'
+    else:
+      classes='r' # rook? 
+
+    return classes
---
+++ board_parser_test.py
@@ -0,0 +1,40 @@
+import board_parser
+import matplotlib.pyplot as plt
+from time import sleep
+from PIL import Image
+import numpy as np
+
+#fp's
+cpaib_rel_path = 'Chess-Playing-AI-Bot/'
+test_data_path = 'test_data/new_'
+test_iter = '0'
+
+parser = board_parser.BoardParser()
+board = np.zeros((1,800,800,3))
+
+# Tests the chess board classifier
+for r in range(1,65):
+  img = Image.open(cpaib_rel_path + test_data_path + test_iter + '/opencv_frame1_'+str(r)+'.jpg')
+  img = np.array(img)
+  img = np.reshape(img,(100,100,3))
+  #y = image.img_to_array(img)
+
+  #print(y.shape)
+  #print(img.shape)
+
+  # plt.imshow(img)
+  # plt.show()
+  img = np.reshape(img,(1,100,100,3))
+  # print(img.shape)
+
+  
+  #pred = parser.parse_single_square(img)
+  #print(pred)
+
+  x = ((r-1) // 8 + 1) * 100
+  y = ((r-1) % 8 + 1) * 100
+
+  board[0][x-100:x, y-100:y] = img
+  
+pred = parser.parse_board_image(board)
+print(pred)
